0793e69a668ddbcc407bd5d76bc62380ea3e1617
dgssdagdg <100527555+dgssdagdg@users.noreply.github.com>
2022-12-27 19:46:16 +0300

    add

--- js/script.js
+++ js/script.js
@@ -16,6 +16,7 @@ const swiperTwo = new Swiper('.price-slider', {
 });
 const swiperThree = new Swiper('.clients-sliders', {
 	loop: true,
+	slidesPerView: 'auto',
 	breakpoints: {
 		320: {
 		  spaceBetween: 55
@@ -40,7 +41,41 @@ const swiperThree = new Swiper('.clients-sliders', {
 	// 	delay: 3000,
 	//   },
 });
-
+const swiperFour = new Swiper('.comment-sliders', {
+	loop: true,
+	slidesPerView: '1',
+	speed: 800,
+	breakpoints: {
+		320: {
+		  spaceBetween: 55
+		},
+		340: {
+			spaceBetween: 35
+		  },
+		400: {
+			spaceBetween: 45
+		},
+		767: {
+		  spaceBetween: 100
+		},
+		992: {
+			spaceBetween: 120
+		},
+		1200: {
+			spaceBetween: 135
+		}
+	},
+	navigation: {
+		nextEl: '.comment-button-next',
+		prevEl: '.comment-button-prev',
+	  },
+});
+const swiperfive = new Swiper('.partners-sliders', {
+	loop: true,
+	slidesPerView: 'auto',
+	speed: 800,
+	spaceBetween: 42,
+});
 let body = document.body;
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
@@ -48,6 +83,63 @@ menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
 	body.classList.toggle('overflow');
+})
+window.addEventListener('click', function(event) {
+	let mkadMain = document.querySelector('.mkad-form-selects');
+	let arrow = document.querySelector('.mkad-form-select-arrow');
+	if (event.target.closest('.mkad-form-select')) {
+		mkadMain.classList.toggle('mkad-form-selects-active');
+		arrow.classList.toggle('arrow-active');
+	}
+	let mkadPrev = document.querySelector('.mkad-form-select');
+	let mkadPrevText = mkadPrev.querySelector('.mkad-form-select-text');
+	let mkadPrevPrice = mkadPrev.querySelector('.mkad-form-select-price');
+	if (event.target.closest('.mkad-form-list-link-two')) {
+
+		let mkadNext = document.querySelector('.mkad-form-list-link-two');
+		let mkadNextText = mkadNext.querySelector('.mkad-form-select-text').innerText;
+		let mkadNextPrice = mkadNext.querySelector('.mkad-form-select-price').innerText;
+		mkadPrevText.innerText = mkadNextText;
+		mkadPrevPrice.innerText = mkadNextPrice;
+		mkadMain.classList.remove('mkad-form-selects-active');
+		arrow.classList.remove('arrow-active');
+	}
+	if (event.target.closest('.mkad-form-list-link-one')) {
+
+		let mkadNext = document.querySelector('.mkad-form-list-link-one');
+		let mkadNextText = mkadNext.querySelector('.mkad-form-select-text').innerText;
+		let mkadNextPrice = mkadNext.querySelector('.mkad-form-select-price').innerText;
+		mkadPrevText.innerText = mkadNextText;
+		mkadPrevPrice.innerText = mkadNextPrice;
+		mkadMain.classList.remove('mkad-form-selects-active');
+		arrow.classList.remove('arrow-active');
+	}
+
+	let documentItemOne = document.querySelector('.document-btn-one');
+	let documentBlockOne= document.querySelector('.document-block-one');
+	let documentItemTwo = document.querySelector('.document-btn-two');
+	let documentBlockTwo= document.querySelector('.document-block-two');
+	if (event.target.closest('.document-btn-one') && documentItemTwo.closest('.document-btn-active')) {
+		let item = event.target;
+		item.classList.add('document-btn-active');
+		let block = document.querySelector('.document-block-one');
+		block.classList.add('document-block-active');
+		documentItemTwo.classList.remove('document-btn-active');
+		documentBlockTwo.classList.remove('document-block-active');
+	}
+	if (event.target.closest('.document-btn-two') && documentItemOne.closest('.document-btn-active')) {
+		let item = event.target;
+		item.classList.add('document-btn-active');
+		let block = document.querySelector('.document-block-two');
+		block.classList.add('document-block-active');
+		documentItemOne.classList.remove('document-btn-active');
+		documentBlockOne.classList.remove('document-block-active');
+	}
+
+	if (event.target.closest('.questions-item-title')) {
+		let questionsItem = event.target.closest('.questions-item');
+		questionsItem.classList.toggle('questions-item-active');
+	}
 })
 
 window.onscroll = function(){
@@ -61,4 +153,48 @@ window.onscroll = function(){
 		BlkStyle.classList.remove('header-fix');
 		main.classList.remove('main-p');
 	}
-	 }
+}
+
+function getTimeRemaining(endtime) {
+	var t = Date.parse(endtime) - Date.parse(new Date());
+	var seconds = Math.floor((t / 1000) % 60);
+	var minutes = Math.floor((t / 1000 / 60) % 60);
+	var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+	var days = Math.floor(t / (1000 * 60 * 60 * 24));
+	return {
+	  total: t,
+	  days: days,
+	  hours: hours,
+	  minutes: minutes,
+	  seconds: seconds
+	};
+  }
+   
+  function initializeClock(id, endtime) {
+	var clock = document.getElementById(id);
+	var daysSpan = clock.querySelector(".days");
+	var hoursSpan = clock.querySelector(".hours");
+	var minutesSpan = clock.querySelector(".minutes");
+	var secondsSpan = clock.querySelector(".seconds");
+   
+	function updateClock() {
+	  var t = getTimeRemaining(endtime);
+   
+	  if (t.total <= 0) {
+		clearInterval(timeinterval);
+		var deadline = new Date(Date.parse(new Date()) + 19 * 13 * 1000);
+		initializeClock('countdown', deadline);
+	  }
+   
+	  daysSpan.innerHTML = t.days;
+	  hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
+	  minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
+	  secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
+	}
+   
+	updateClock();
+	var timeinterval = setInterval(updateClock, 1000);
+  }
+   
+  var deadline = new Date(Date.parse(new Date()) + 19 * 63 * 1000);
+  initializeClock("countdown", deadline);
